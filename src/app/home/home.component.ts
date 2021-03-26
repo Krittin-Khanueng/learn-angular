f04988ab9a58ef3dff2af03605d0b4a3e45ce48f
Krittin <6110210013@psu.ac.th>
2021-03-26 08:51:01 +0700

    test

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html'
+})
+export class HomeComponent{
+    @Input()
+    title:string;
+    constructor() {
+    }
+}
